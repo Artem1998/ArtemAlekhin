dd67fdd99a7517ac11e7d8af8928123988ce06bd
Artem Alekhin <arty-hero@yandex.ru>
2017-02-23 23:18:17 -0400

    My personal website

--- js/form_to_email.php
+++ js/form_to_email.php
@@ -1,11 +1,16 @@
+<?php
 /**
- * Created by Artem on 2/23/2017.
+ * Created by PhpStorm.
+ * User: Artem
+ * Date: 2/23/2017
+ * Time: 10:28 PM
  */
-
 $name = $_POST['name'];
 $email_from = $_POST['email'];
 $message = $_POST['message'];
 
 $to = "alekhinartem874@gmail.com";
 $headers = "From: $email_from \r\n";
+$email_subject = "Job";
+
 mail($to,$email_subject,$email_body,$headers);
